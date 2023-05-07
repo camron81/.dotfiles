5cc552f971215deff74a5ce19781effcf7408290
camron81 <ethancameron81@gmail.com>
2023-05-07 09:32:20 +0100

    removed substitute channel for nonguix

--- channels.scm
+++ channels.scm
@@ -11,14 +11,12 @@
               (openpgp-fingerprint
                "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA"))))
         "https://ci.guix.gnu.org")
-      (channel-with-substitues-available
-        (channel
-          (name 'nonguix)
-          (url "https://gitlab.com/nonguix/nonguix")
-          (branch "master")
-          (introduction
-            (make-channel-introduction
-             "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
-              (openpgp-fingerprint
-               "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5"))))
-        "https://substitutes.nonguix.org"))
+      (channel
+        (name 'nonguix)
+        (url "https://gitlab.com/nonguix/nonguix")
+        (branch "master")
+        (introduction
+          (make-channel-introduction
+           "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
+            (openpgp-fingerprint
+             "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5")))))
