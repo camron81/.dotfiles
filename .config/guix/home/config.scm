76e2299084380319b2ca4c5418d43e96d6a73379
Ethan Cameron <ethancameron81@gmail.com>
2023-05-20 17:56:02 +0100

    added home configuration

--- .config/guix/home/config.scm
+++ .config/guix/home/config.scm
@@ -1,31 +1,33 @@
 (use-modules (gnu home)
+             (gnu home services)
              (gnu home services shells)
              (gnu services)
              (guix gexp))
 
-(define load-user-profiles
+(define bash-profile-text
   "for p in $GUIX_USER_PROFILES/*; do
-       profile=$p/$(basename \"$p\")
-       if [ -f \"$profile\"/etc/profile ]; then
-           GUIX_PROFILE=\"$profile\"
-	   . \"$GUIX_PROFILE\"/etc/profile
-       fi
-       unset profile
-   done")
+    profile=$p/$(basename \"$p\")
+    if [ -f \"$profile\"/etc/profile ]; then
+        GUIX_PROFILE=\"$profile\"
+        . \"$GUIX_PROFILE\"/etc/profile
+    fi
+    unset profile
+done
+
+[ \"$(tty)\" = \"/dev/tty1\" ] && exec sway")
 
 (home-environment 
   (services 
     (list 
-       (service home-bash-service-type
-         (home-bash-configuration
-           (environment-variables
-             '(("XDG_CONFIG_HOME" . "$HOME/.config")
-               ("XDG_DATA_HOME" . "$HOME/.local/share")
-               ("XDG_STATE_HOME" . "$HOME/.local/state")
-               ("GUIX_USER_PROFILES" . "$XDG_DATA_HOME/guix-profiles")))
-           (aliases
-            '(("cp" . "cp -riv")
-              ("mv" . "mv -iv")
-              ("rm" . "rm -iv")))
-          (bashrc
-            (list (plain-file "bash_config" load-user-profiles))))))))
+      ; $GUIX_USER_PROFILES needs set before .bash_profile is sourced
+      (simple-service 'guix-user-profile-env-var-service
+                      home-environment-variables-service-type
+                      '(("GUIX_USER_PROFILES" . "$XDG_DATA_HOME/guix-profiles")))
+      (service home-bash-service-type
+          (home-bash-configuration
+            (aliases
+             '(("cp" . "cp -riv")
+               ("mv" . "mv -iv")
+               ("rm" . "rm -iv")))
+           (bash-profile (list (plain-file "bash-profile"
+                                           bash-profile-text))))))))
